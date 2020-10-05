36dabe6ca3e4d21cda38a0d13648e6f5a805eb62
Kevin Barreiro <kevinbarreirooo729@gmail.com>
2020-10-05 12:50:58 -0400

    Fixed cookie feature bugs.

--- js/index.js
+++ js/index.js
@@ -1,32 +1,47 @@
-var username, cookieArr;
+var username;
 
 var cookieExpiration = new Date().toUTCString + 60 * 60000;
 // 60 is the amount of minutes in 1 hour. 
 // 60000 is the amount of milleseconds in 1 minute. 
 // Thus, cookiesExpiration is 1 hour into the future. 
 
-if (document.cookie.split('; ').length > 0) {
-    // This portion runs if the user has visited the page. 
+// Test for the existence of cookies and proceed accordingly. 
+if (document.cookie.split('; ')[0].split('=')[0] == 'username') {
+    // This portion runs if the user has visited the page, i.e. cookies exist.  
 
-    // Gets the username from the username cookie. 
-    username = document.cookie.split('; ').find(element => element.startsWith('username')).split('=')[1];
-    // Gets the visit count from the visitCount cookie. 
-    var visitCountCookie = document.cookie.split('; ').find(element => element.startsWith('visitCount')).split('=')[1];
-    // Display a welcome message and the number of visits in index.html. 
-    document.getElementById('welcome-message').innerHTML += 'Greetings, ' + username +
+    // Store the cookie in a key=value array for each cookie. 
+    var cookies = document.cookie.split('; ');
+    alert('cookies: ' + cookies);
+    var usernameCookie = cookies[0].split('=');
+    var visitCountCookie = cookies[1].split('=');
+
+    // Get the username and visit count. 
+    var cookieUsername, visitCount;
+    cookieUsername = usernameCookie[1];
+    visitCount = visitCountCookie[1];
+
+    // Increase the visit count by 1. 
+    visitCount = parseInt(visitCount);
+    visitCount++;
+    document.cookie = 'visitCount=' + visitCount;
+
+    // Display the greetings and the visit count. 
+    document.getElementById('welcome-message').innerHTML += 'Greetings, ' + cookieUsername +
         '!<br>This is visit number ' + visitCount + '.';
 } else {
-    // This portion runs if the user has never visited the page. 
+    // This portion runs if the user has never visited the page. i.e. cookies don't exist. 
 
     // Prompts the user for their name.
     username = prompt('Enter your name.');
     // Creates a username cookie. 
-    document.cookie = 'username=' + username + "; expires=" + cookieExpiration;
+    document.cookie = 'username=' + username + '; expires=' + cookieExpiration;
+    alert(document.cookie);
     // Creates a visit count cookie. 
-    document.cookie = 'visitCount=1;' + " expires=" + cookieExpiration;
+    document.cookie = 'visitCount=1;' + ' expires=' + cookieExpiration;
+    alert(document.cookie);
     // Display a welcome message in index.html. 
     document.getElementById('welcome-message').innerHTML += 'Greetings, ' + username + '!';
-}
+};
 
 // Algorithm Pseudocode - 
 // if its the users first time on the home page
