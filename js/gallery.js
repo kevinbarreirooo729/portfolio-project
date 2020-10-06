0ac32d4034dad324b9a391b5a68d4091cad9f553
Kevin Barreiro <kevinbarreirooo729@gmail.com>
2020-10-05 20:06:40 -0400

    Fixed a bug in the image gallery within gallery.html that was causing images to disappear when the previous button was clicked.

---
+++ js/gallery.js
@@ -0,0 +1,37 @@
+var totalPics = 0,
+    currentPic = 0;
+var gallery = new Array();
+
+document.getElementById('next-button').onclick = nextPicture;
+document.getElementById('previous-button').onclick = previousPicture;
+
+function startGallery() {
+    var imgTagArr = document.getElementsByTagName('img');
+    for (let i = 0; i < imgTagArr.length; i++) {
+        gallery[i] = imgTagArr[i];
+        if (totalPics == 0) {
+            imgTagArr[i].style.display = 'block';
+        } else {
+            imgTagArr[i].style.display = 'none';
+        };
+        totalPics++;
+    };
+};
+
+function nextPicture() {
+    gallery[currentPic].style.display = 'none';
+    currentPic++;
+    if (currentPic >= totalPics) currentPic = 0;
+    gallery[currentPic].style.display = 'block';
+};
+
+function previousPicture() {
+    gallery[currentPic].style.display = 'none';
+    currentPic--;
+    if (currentPic <= -1) {
+        currentPic = gallery.length - 1; 
+    };
+    gallery[currentPic].style.display = 'block';
+};
+
+window.onload = startGallery;
